8c2ba04aaff87ff782e4c9f5d6776704cd631739
Jesús Efrain Pluma Garcia (JepG) <168058829+JepGGGG@users.noreply.github.com>
2024-05-23 10:47:36 -0600

    Create Ejec_num_4.cpp

---
+++ Ejec_num_4.cpp
@@ -0,0 +1,13 @@
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    char primero, ultimo;
+    cout << "Introduzca su primera y ultima inicial: \n";
+    cin >> primero;
+    cin >> ultimo;
+    cout << "Hola " << primero << ultimo;
+
+    return 0;
+}
