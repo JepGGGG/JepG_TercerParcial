42fdc89ff3d1bdc3da6cffdac7b5ac6a7f2b3aa4
Jesús Efrain Pluma Garcia (JepG) <168058829+JepGGGG@users.noreply.github.com>
2024-05-23 10:52:48 -0600

    Create Ejerc_num_9.cpp

---
+++ Ejerc_num_9.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    cout << "El tamaño de variables de coma flotante es: " << sizeof(float);
+    cout << "Bytes \n";
+    cout << "EL tamaño de variables de doble precision es: " << sizeof(double);
+    cout << "Bytes\n";
+
+        return 0;
+}
