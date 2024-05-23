3318e0d64ceb5e150c1f7a10fd480606f62ca27f
Jesús Efrain Pluma Garcia (JepG) <168058829+JepGGGG@users.noreply.github.com>
2024-05-23 10:44:50 -0600

    Create Ejec_num_1.cpp

---
+++ Ejec_num_1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int unidad, subtotal, precio;
+    cin >> unidad;
+    cout << "Cantidad de unidades: \n" << unidad;
+    cin >> precio;
+    cout << "Precio: \n"<< precio;
+    subtotal = unidad * precio;
+    cout <<subtotal;
+
+    return 0;
+}
