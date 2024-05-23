37a8fb36605df9be52bcad723d262a5d9fc8cc97
Jesús Efrain Pluma Garcia (JepG) <168058829+JepGGGG@users.noreply.github.com>
2024-05-23 10:49:01 -0600

    Create Ejerc_num_5.cpp

---
+++ Ejerc_num_5.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+#define t "Tambor de hoja lata"
+int main(){
+    char st[21] = "Todo puede hacerse";
+    puts(t);
+    cout << "Permiso para salir de la foto";
+    cout << st;
+    cout << st;
+
+    return 0;
+}
