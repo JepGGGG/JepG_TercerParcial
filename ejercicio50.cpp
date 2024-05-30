24d5527ae20ce7bb352cb02203641a25b2888273
JepG <chuypg1520@gmail.com>
2024-05-29 21:07:30 -0600

    Cambio libreria iostream

--- ejercicio50.cpp
+++ ejercicio50.cpp
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include <iostream>
 
 #define FILAS 4
 #define COLUMNAS 3
@@ -7,15 +7,13 @@ int main() {
   int tabla[FILAS][COLUMNAS];
   int suma_columnas[COLUMNAS];
 
-  // Leer valores de la tabla
   for (int fila = 0; fila < FILAS; fila++) {
     for (int columna = 0; columna < COLUMNAS; columna++) {
-      printf("Introduce un valor para la fila %d, columna %d: ", fila + 1, columna + 1);
-      scanf("%d", &tabla[fila][columna]);
+      std::cout << "Introduce un valor para la fila " << fila + 1 << ", columna " << columna + 1 << ": ";
+      std::cin >> tabla[fila][columna];
     }
   }
 
-  // Calcular suma de cada columna
   for (int columna = 0; columna < COLUMNAS; columna++) {
     suma_columnas[columna] = 0;
     for (int fila = 0; fila < FILAS; fila++) {
@@ -23,7 +21,6 @@ int main() {
     }
   }
 
-  // Encontrar la mayor suma de columna
   int mayor_suma = suma_columnas[0];
   for (int columna = 1; columna < COLUMNAS; columna++) {
     if (suma_columnas[columna] > mayor_suma) {
@@ -31,8 +28,8 @@ int main() {
     }
   }
 
-  // Mostrar la mayor suma de columna
-  printf("La mayor suma de columna es: %d\n", mayor_suma);
+ 
+  std::cout << "La mayor suma de columna es: " << mayor_suma << std::endl;
 
   return 0;
 }
