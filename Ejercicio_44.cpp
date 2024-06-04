eca34fc75df51659b15432afe178c940972c76d6
Jesús Efrain Pluma Garcia (JepG) <168058829+JepGGGG@users.noreply.github.com>
2024-06-03 21:54:34 -0600

    Update Ejercicio_44.cpp

--- Ejercicio_44.cpp
+++ Ejercicio_44.cpp
@@ -1,1 +1,16 @@
 
+#include <iostream>
+#include <math.h>
+using namespace std;
+int main()
+{
+    int x, y, z = 0;
+    cout << "Ingrese un numero: " << endl;
+    cin >> x;
+    cout << "Ingrese el exponente: " << endl;
+    cin >> y;
+    z = pow(x, y);
+    cout << "El resultado de la potencia es de: " << z << endl;
+
+    return 0;
+}
