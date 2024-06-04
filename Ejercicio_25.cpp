5963f43e534ebfd39c9357c9d31830581c57b11b
Jesús Efrain Pluma Garcia (JepG) <168058829+JepGGGG@users.noreply.github.com>
2024-06-03 21:44:13 -0600

    Update Ejercicio_25.cpp

--- Ejercicio_25.cpp
+++ Ejercicio_25.cpp
@@ -1,33 +1,15 @@
-#include <iostream>
+#include<iostream>
 using namespace std;
 
-int main() {
-  int numero, menor, mayor;
-  int i;
-  int suma=0;
-  menor=0;
-  mayor=0;
+int main(){
+    int b,h;
+    float r = 0;
+    cout << "Introduzca la base del triangulio: " << endl;
+    cin >> b;
+    cout << "Introduzca la altura del triangulo: " << endl; 
+    cin >> h;
+    r = (b*h)/2;
+    cout << "Un triangulo de altura " << h << ", y de base " << b << ", tiene un area de: " << r << endl;
 
-cout<<"dime 10 numeros";
-cin>>numero;
-menor=numero;
-mayor=numero;
-suma+=numero;
-
-for(int i=1;i<10;i++){
-cin>>numero;
-suma+=numero;
-
-if(numero<menor){;
-menor=numero;
-}
-if(numero>mayor){
-mayor=numero;
-}
-}
-cout<<"el numero mayor es"<<mayor<<endl;
-cout<<"el numero menor es"<<menor<<endl;
-cout<<"la suma de los numeros es"<<suma<<endl;
-
-return 0;
+    return 0;
 }
