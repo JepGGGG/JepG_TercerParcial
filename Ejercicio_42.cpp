928afc65b14affdab76e794f950b4e21ae28f2bf
Jesús Efrain Pluma Garcia (JepG) <168058829+JepGGGG@users.noreply.github.com>
2024-06-03 21:53:41 -0600

    Update Ejercicio_42.cpp

--- Ejercicio_42.cpp
+++ Ejercicio_42.cpp
@@ -1,21 +1,9 @@
-#include <iostream>
-#include <cstdlib> // Para usar la función rand()
-
+#include<iostream>
 using namespace std;
-
-int main() {
-  // Creamos el arreglo y le pasamos el tamaño ingresado
-  int num[10];
-
-  // Llenamos el arreglo con un ciclo for
-  for (int i=0; i<10; i++) {
-    num[i] = rand() % 100 + 1; // Generamos un número aleatorio entre 100 y 199
-  }
-
-  // Imprimimos los datos con un ciclo for
-  for (int i=0; i<10; i++) {
-    cout << "Los números aleatorios son: " << num[i] << endl;
-  }
-
-  return 0;
+int main(){
+    int i;
+    for(i  = 100; i = 0; i--){
+        cout << i << endl;
+    }
+    return 0;
 }
