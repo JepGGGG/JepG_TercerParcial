571028cb693f1caa105bebce62fa3f588e26e446
Jesús Efrain Pluma Garcia (JepG) <168058829+JepGGGG@users.noreply.github.com>
2024-06-03 21:50:08 -0600

    Update Ejercicio_31.cpp

--- Ejercicio_31.cpp
+++ Ejercicio_31.cpp
@@ -1,21 +1,15 @@
 #include <iostream>
-
 using namespace std;
-
-int main() {
-  // Creamos el arreglo 'num' de 5 posiciones
-  int num[5];
-
-  // Pedimos datos para cada posición del arreglo
-  for (int i = 0; i < 5; i++) {
-    cout << "Digite un número para la posición " << i << ": ";
-    cin >> num[i];
-  }
-
-  // Imprimimos los datos del arreglo
-  for (int i = 0; i < 5; i++) {
-    cout << "El dato en la posición " << i << " es: " << num[i] << endl;
-  }
-
-  return 0;
+int main(){
+    int x, min, i;
+    for (i = 0; i < 5; i++)
+    {
+        cout << "Ingrese el numero " << i <<":" << endl; 
+        cin >> x;
+        if(x < min){
+            min = x;
+        }
+    }
+    cout << "El numero menor es: " << min << endl;
+    return 0;
 }
