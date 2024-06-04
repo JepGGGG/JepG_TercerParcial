c90c946e1f3afdc94ac395256450e8448c205639
Jesús Efrain Pluma Garcia (JepG) <168058829+JepGGGG@users.noreply.github.com>
2024-06-03 21:50:22 -0600

    Update Ejercicio_32.cpp

--- Ejercicio_32.cpp
+++ Ejercicio_32.cpp
@@ -1,31 +1,26 @@
-#include <iostream>
-
+#include<iostream>
 using namespace std;
-
-int main() {
-  // Definimos la matriz con 2 filas y 2 columnas
-  int matriz[2][2];
-
-  // Asignamos valores a los elementos de la matriz
-  matriz[0][0] = -10; // Fila 1, Columna 1
-  matriz[0][1] = -20; // Fila 1, Columna 2
-  matriz[1][0] = -30; // Fila 2, Columna 1
-  matriz[1][1] = -40; // Fila 2, Columna 2
-
-  // Imprimimos los datos de la matriz
-  cout << "Matriz:" << endl;
-  for (int i = 0; i < 2; ++i) {
-    for (int j = 0; j < 2; ++j) {
-      cout << matriz[i][j] << " ";
+int main(){
+    int x1,x2,x3, i, j, tem;
+    cout << "Ingrese 3 numeros: " << endl; 
+    cin >> x1;
+    cin >> x2;
+    cin >> x3;
+    int arr[] = {x1,x2,x3};
+    for ( i = 0; i < 2; i++){
+        for ( j = 0; j < 2; j++){
+            if(arr[j]<arr[j+1]){
+                tem = arr[j];
+                arr[j] = arr[j +1];
+                arr[j+1] = tem;
+            }
+        }
+        
     }
-    cout << endl;
-  }
+    cout << "Los numeros ordenados de mayor a menor son: " << endl;
+    for (i= 0; i < 3; i++){
+        cout << arr[i] << endl;
+    }
 
-  // Impresión de datos específicos
-  cout << "\nEl dato en la fila 1, columna 1 es: " << matriz[0][0] << endl;
-  cout << "El dato en la fila 1, columna 2 es: " << matriz[0][1] << endl;
-  cout << "El dato en la fila 2, columna 1 es: " << matriz[1][0] << endl;
-  cout << "El dato en la fila 2, columna 2 es: " << matriz[1][1] << endl;
-
-  return 0;
+    return 0;
 }
