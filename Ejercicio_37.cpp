0831d515300bee95afa37624c0eba7d2c577092a
Jesús Efrain Pluma Garcia (JepG) <168058829+JepGGGG@users.noreply.github.com>
2024-06-03 21:51:45 -0600

    Update Ejercicio_37.cpp

--- Ejercicio_37.cpp
+++ Ejercicio_37.cpp
@@ -1,23 +1,44 @@
 #include <iostream>
-
+#include <math.h>
 using namespace std;
+int main()
+{
+    int x, b, B, h;
+    float z;
+    cout << "CALCULO DE AREAS\n";
+    cout << "____________________\n";
+    cout << "1.Calcular el area de un triangulo.\n";
+    cout << "2.Calcular el area de un trapecio.\n";
+    cout << "3.Calcular el area de un rectangulo.\n";
+    cout << "Que desea saber???";
+    cin >> x;
+    switch (x)
+    {
+    case 1:
+        cout << "Ingrese la base primero y despues la haltura: " << endl;
+        cin >> b;
+        cin >> h;
+        z = (b * h) / 2;
+        cout << "El area del triangulo es: " << z << endl;
+        break;
+    case 2:
+        cout << "Ingrese primero la base mayor, despue la base menor y al final la haltura: " << endl;
+        cin >> B; 
+        cin >> b; 
+        cin >> h;
+        z = ((B + b) * h) / 2;
+        cout << "El area del trapecio es de: " << z << endl;
+        break;
+    case 3:
+        cout << "Ingrese el largo y despues el ancho del rectangulo: " << endl;
+        cin >> b; 
+        cin >> h;
+        z = b * h;
+        cout << "El area del rectangulo es: " << z << endl;
+        break;
+    default:
+        break;
+    }
 
-int main() {
-  // Creamos dos arreglos en una sola instrucción
-  string arreglo1[2] = {"Fernando", "Oscar"};
-  int arreglo2[3] = {74, 56, 10};
-
-  // Imprimimos todos los datos del arreglo 1
-  cout << "Arreglo 1" << endl;
-  for (int i = 0; i < 2; i++) {
-    cout << "El dato en la posición " << i << " es: " << arreglo1[i] << endl;
-  }
-
-  // Imprimimos los datos del arreglo 2
-  cout << "\nArreglo 2" << endl;
-  for (int i = 0; i < 3; i++) {
-    cout << "El dato en la posición " << i << " es: " << arreglo2[i] << endl;
-  }
-
-  return 0;
+    return 0;
 }
