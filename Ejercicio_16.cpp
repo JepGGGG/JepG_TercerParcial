84678e8dffdc7ef853493ce7aaeb043f6c153436
Jesús Efrain Pluma Garcia (JepG) <168058829+JepGGGG@users.noreply.github.com>
2024-06-03 21:41:47 -0600

    Create Ejercicio_16.cpp

---
+++ Ejercicio_16.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int edad, mayoredad, juvenil;
+    cin >> edad;
+    mayoredad = (edad > 18);
+    juvenil = (edad > 15) && (edad <= 18);
+    cout << "";
+
+    return 0;
+}
