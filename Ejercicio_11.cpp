33748e526100809845b35b8f959e30bb969998d8
Jesús Efrain Pluma Garcia (JepG) <168058829+JepGGGG@users.noreply.github.com>
2024-06-03 21:39:53 -0600

    Update Ejercicio_11.cpp

--- Ejercicio_11.cpp
+++ Ejercicio_11.cpp
@@ -1,8 +1,14 @@
+
 #include <iostream>
 using namespace std;
-int main(){
-	int i;
-	for(i=1;i<=100; i++){
-		cout<<i<<"\n";
-	}
+int main()
+{
+    int x, y;
+    float z1 = 0;
+    cout << "Ingrese 2 numeros: " << endl;
+    cin >> x >> y;
+    z1 = x / y;
+    cout << z1;
+
+    return 0;
 }
