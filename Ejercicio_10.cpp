bbc00b14941be0c8f6149c84a34040b15e53ec87
Jesús Efrain Pluma Garcia (JepG) <168058829+JepGGGG@users.noreply.github.com>
2024-06-03 21:39:29 -0600

    Update Ejercicio_10.cpp

--- Ejercicio_10.cpp
+++ Ejercicio_10.cpp
@@ -1,12 +1,9 @@
 #include <iostream>
-
 using namespace std;
+int main(){
+    int x;
+    x = 4/0;
+    cout<<x;
 
-int main() {
-  for (int i = 0; i <= 100; i += 2) {
-    cout << i << "\t";
-  }
-  cout << "Muchas gracias por utilizar\n";
-
-  return 0;
+    return 0;
 }
