db946843150a5e99f3517416c71dd6faa003c145
Jesús Efrain Pluma Garcia (JepG) <168058829+JepGGGG@users.noreply.github.com>
2024-06-03 21:43:59 -0600

    Update Ejercicio_24.cpp

--- Ejercicio_24.cpp
+++ Ejercicio_24.cpp
@@ -1,22 +1,17 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
 int main(){
+    int dinero;
+    float interes, ci, cf = 0;
+    cout << "Ingrese el dinero depositado: " << endl;
+    cin >> dinero;
+    cout << "Ingrese el tipo de interes: " << endl;
+    cin >> interes;
+    ci = dinero * interes;
+    cf = dinero + ci;
+    cout << "El dinero de interes es de: $" << ci;
+    cout << "El sueldo bruto es de: $" << cf;
 
-int numero,potencia,exponente;
-
-cout<<"dime el numero que quieres elevar";
-cin>>numero;
-cout<<"dime el exponente";
-cin>>exponente;
-potencia=1;
-
-for(int i=0;i< exponente;i++){;
-
-potencia*=numero;
-}
- 
-cout<<numero<< "elevado a la potencia" <<exponente<< "es" <<potencia<<endl;
-
-return 0;
+    return 0;
 }
