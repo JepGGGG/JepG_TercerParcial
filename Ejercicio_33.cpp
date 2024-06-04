ce194b5df7b4adac10bfba50bce0e4276c2c4a05
Jesús Efrain Pluma Garcia (JepG) <168058829+JepGGGG@users.noreply.github.com>
2024-06-03 21:50:36 -0600

    Update Ejercicio_33.cpp

--- Ejercicio_33.cpp
+++ Ejercicio_33.cpp
@@ -1,46 +1,18 @@
 #include <iostream>
-
 using namespace std;
-
-int main() {
-  // Solicitamos el número de filas
-  int f;
-  cout << "Digite el número de filas: ";
-  cin >> f;
-
-  // Solicitamos el número de columnas
-  int c;
-  cout << "Digite el número de columnas: ";
-  cin >> c;
-
-  // Creamos la matriz dinámica
-  int** matriz = new int*[f]; // Reservamos memoria para las filas
-  for (int i = 0; i < f; ++i) {
-    matriz[i] = new int[c]; // Reservamos memoria para las columnas de cada fila
-  }
-
-  // Llenamos la matriz con datos ingresados por el usuario
-  for (int i = 0; i < f; ++i) {
-    for (int j = 0; j < c; ++j) {
-      cout << "Digite el dato para la fila " << i + 1 << ", columna " << j + 1 << ": ";
-      cin >> matriz[i][j];
+int main()
+{
+    char y;
+    cout << "Ingrese una letra: " << endl;
+    cin >> y;
+    if (y == 'a' || y == 'e' || y == 'i' || y == 'o' || y == 'u')
+    {
+        cout << "Es una vocal" << endl;
     }
-  }
-
-  // Mostramos los datos de la matriz
-  cout << "\nLos datos de la matriz son:\n";
-  for (int i = 0; i < f; ++i) {
-    for (int j = 0; j < c; ++j) {
-      cout << matriz[i][j] << " ";
+    else
+    {
+        cout << "Es una consonante" << endl;
     }
-    cout << endl;
-  }
 
-  // Liberamos la memoria asignada dinámicamente
-  for (int i = 0; i < f; ++i) {
-    delete[] matriz[i];
-  }
-  delete[] matriz;
-
-  return 0;
+    return 0;
 }
